d20faa6b0a3c7c18f305c276db1f51b6e913978f
Midhun Harikumar <midhunhk@gmail.com>
2016-03-22 10:18:44 +0530

    updates to read data in JSON format

--- app-base/moonshine.js
+++ app-base/moonshine.js
@@ -1,21 +1,22 @@
 /**
- * 	Moonshine
- *	A multi node graph implementation using HTML5 Canvas
+ * Moonshine
+ * A multi node graph implementation using HTML5 Canvas
  * 
- * 	based on https://github.com/boundary/html5-node-diagram
- * 	Colors: http://www.colorhexa.com/0099cc
+ * based on https://github.com/boundary/html5-node-diagram
  **/
- 
-// Init
-var NODE_DIMENSIONS = {
-	w: 120,
-	h: 50
-};
-var stage,
-	Node = window.Node,
-	Segment = window.Segment;
 
-var ENABLE_EDIT_TITLE_ON_CLICK = true;
+// Configurations
+var CONFIG = {
+    width: 120,
+    height: 50,
+    enableEditTitleOnClick: false
+};
+
+// internal varaibles
+var stage;
+var Node = window.Node;
+var Segment = window.Segment;
+var highlightedNode;
 
 /**
  * Initialize and run the application
@@ -35,15 +36,19 @@ var ENABLE_EDIT_TITLE_ON_CLICK = true;
 	initialize();
 }(jQuery, window));
 
+//-------------------------------------------------------------------
+// Utility Methods
+//-------------------------------------------------------------------
+
 // Creates a node based on a template
 function createNode(nodeTitle, nodeId, nodeX, nodeY){
-	return new Node({ 
+	return new Node({
 		title: nodeTitle,
 		id: nodeId,
 		filepath: 'placeholder',
 		stage: stage,
-		w: NODE_DIMENSIONS.w,
-		h: NODE_DIMENSIONS.h,
+		w: CONFIG.width,
+		h: CONFIG.height,
 		x: nodeX,
 		y: nodeY,
 		events: {
@@ -73,7 +78,13 @@ function createNode(nodeTitle, nodeId, nodeX, nodeY){
 					}
 					
 					// allow the title to be modified if enabled
-					if(ENABLE_EDIT_TITLE_ON_CLICK){
+					if(CONFIG.enableEditTitleOnClick){
+                        			// Reset the border for the previously highlighted node
+                				if(undefined != highlightedNode){
+                            				highlightedNode.css('border-color', '#ffffff');
+                        			}
+                        
+                        			// Highlight this node and setup a listener for keypress
 						$node.css('border-color', '#ec461c');
 						$node.bind("keypress", function(e){
 							$thisNode = $(this);
@@ -89,8 +100,11 @@ function createNode(nodeTitle, nodeId, nodeX, nodeY){
 							$thisNode.children().first().text(text);
 							
 						});
+						
+						// Save this node as the highlighted node
+                        			highlightedNode = $node;
 					}
-				}else{
+				} else {
 					// Do stuff for non selected state
 					$node.data( 'nodeSelected', 'notselected' );
 					
@@ -99,9 +113,10 @@ function createNode(nodeTitle, nodeId, nodeX, nodeY){
 						var includesegmentid = '#' + includesegments[i].id;
 					}
 					
-					if(ENABLE_EDIT_TITLE_ON_CLICK){
+					if(CONFIG.enableEditTitleOnClick){
 						// Reset the node highlight signifying edit
 						$node.css('border-color', '#ffffff');
+                        
 						// Stop edit when the node is clicked again
 						$node.unbind("keypress");
 						$node.data( 'editStarted', 'false');
@@ -112,10 +127,56 @@ function createNode(nodeTitle, nodeId, nodeX, nodeY){
 	}).attach();
 }
 
+// Prevent backspace key from navigating out of the app
+$(document).on("keydown", function (e) {
+    if (e.which === 8 && !$(e.target).is("input, textarea")) {
+        e.preventDefault();
+    }
+});
+
 /**
- * Utility method that creates and attaches a segment between two nodes
+ * Create and attach a segment between two given nodes
  */
 function createAndAttachSegment(originNode, destinationNode){
 	var segmentId = "includesegment-" + originNode.id + "-" + destinationNode.id;
 	new Segment({h: 5, id:segmentId, stage: stage, origin: originNode, destination: destinationNode}).attach();
+}
+
+/**
+ * Creates all the nodes. All created nodes are public. 
+ * Generated code can be placed in this method and the links should be added in
+ * the createLinks() method
+ */
+function createNodes() {
+    // Dictionary of all nodes with the ID as key
+    nodes = [];
+    
+    // List of links in {source, destination} format
+    links = [];
+    
+    // Iterate over the datas defined in data.js
+    $(graphDatas).each( function(){
+        $data = $(this)[0];
+        
+        // create a node from the data and add it to the dictionary
+        nodes[$data._id] = createNode($data.text, $data._id, $data.xPos, $data.yPos);
+        
+        // Store the link references and build links later once we have all the nodes
+        $($data.links).each( function(){
+            links[links.length] = {"source": $data._id, "target": $(this)[0]._id};
+        });
+    });
+}
+
+/**
+ * Cretes the links between the nodes
+ */
+function createLinks() {
+    // Now all the nodes should be available for creating the links
+    $(links).each( function(){
+        var source = nodes[$(this)[0].source];
+        var target = nodes[$(this)[0].target];
+        
+        createAndAttachSegment(source, target);
+    });
 }
