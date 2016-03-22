b181862312261270c5741adad9ffafb100bbfd53
Midhun Harikumar <midhunhk@gmail.com>
2016-03-22 10:14:25 +0530

    data is now formatted in JSON format

--- app-base/data.js
+++ app-base/data.js
@@ -3,37 +3,50 @@
  * 	The data for displaying the graph are defined in this file
  **/
 
-/**
- * Creates all the nodes. All created nodes are public. 
- * Generated code can be placed in this method and the links should be added in
- * the createLinks() method
- */
-function createNodes() {
-	// These nodes are defined as global variables intentionally
-	// so that they can be accessed in other functions
-	node0 = createNode("boston", "node00", 658,348);
-	node1 = createNode("nevada", "node01", 380,519);
-	node2 = createNode("fort worth", "node02", 509,14);
-	node3 = createNode("san jose", "node03", 508,174);
-	node4 = createNode("new york", "node04", 809,127);
-	node5 = createNode("dallas", "node05", 978,175);
-	node6 = createNode("chicago", "node06", 800,250);
-	node7 = createNode("cincinatti", "node07", 100,350);
-	node8 = createNode("florida", "node08", 867,19);
-	node9 = createNode("washington", "node09", 257,249);
-	node10 = createNode("seattle", "node10", 625,250);
-}
- 
-/**
- * Cretes the links between the nodes
- */
-function createLinks() {
-	createAndAttachSegment(node0, node1)
-	createAndAttachSegment(node4, node2)
-	createAndAttachSegment(node5, node3)
-	createAndAttachSegment(node1, node7)
-	createAndAttachSegment(node0, node7)
-	createAndAttachSegment(node3, node2)
-	createAndAttachSegment(node8, node2)
-	createAndAttachSegment(node9, node2)	
-}
+graphDatas = [
+    {
+        text: "Boston", _id: "node00", xPos: 658, yPos: 348,
+        links:[ {"_id": "node01"}, {"_id": "node07"} ]
+    },
+    {
+        text: "Nevada", _id: "node01", xPos: 380, yPos: 519,
+        links:[ {"_id": "node07"} ]
+    },
+    {
+        text: "Fort Worth", _id: "node02", xPos: 509, yPos: 14,
+        links:[ ]
+    },
+    {
+        text: "San Jose", _id: "node03", xPos: 508, yPos: 174,
+        links:[ {"_id": "node02"} ]
+    },
+    {
+        text: "New York", _id: "node04", xPos: 809, yPos: 127,
+        links:[ {"_id": "node02"} ]
+    },
+    {
+        text: "Dallas", _id: "node05", xPos: 978, yPos: 175,
+        links:[ {"_id": "node03"} ]
+    },
+    {
+        text: "Chicago", _id: "node06", xPos: 800, yPos: 250,
+        links:[  ]
+    },
+    {
+        text: "Cincinatti", _id: "node07", xPos: 100, yPos: 350,
+        links:[  ]
+    },
+    {
+        text: "Florida", _id: "node08", xPos: 867, yPos: 19,
+        links:[ {"_id": "node02"} ]
+    },
+    {
+        text: "Washington", _id: "node09", xPos: 257, yPos: 249,
+        links:[ {"_id": "node02"} ]
+    },
+    {
+        text: "Seattle", _id: "node10", xPos: 625, yPos: 250,
+        links:[ ]
+    }
+    
+];
