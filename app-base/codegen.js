ea42b89bd423ecf9a0971e91b163b073364c8efc
Midhun Harikumar <midhunhk@gmail.com>
2016-03-23 10:19:23 +0530

    Generate code for links

--- app-base/codegen.js
+++ app-base/codegen.js
@@ -10,20 +10,49 @@ $(document).ready(function () {
 
     $("#btnCode").click(function () {
         // Let's generate some code!!!
-
+        
+        // Build all relations between nodes
+        var allLinks = [];
+        $('.segment').each( function(){
+            var segments = $(this).attr("id").split("-");
+            var sourceNode = segments[1];
+            var targetNode = segments[2];
+            
+            var links = allLinks[sourceNode];
+            
+            if(links == undefined){
+                allLinks[sourceNode] = targetNode;
+            } else {
+                allLinks[sourceNode] = links + "," + targetNode;
+            }
+        });
+        
         var completeCode = "[ ";
         var nodesLength = $('.node').length;
         $('.node').each(function (index, obj) {
             var $obj = $(obj);
+            var objId = $obj.attr("id");
             
             var code = "    {";
-                code = code + "text: \"" + $obj.children().first().text() + "\",";
-                code = code + "_id:  \"" + $obj.attr("id") + "\", ";
-                code = code + "xPos: " + $obj.position().left + ",";
-                code = code + "yPos: " + $obj.position().top + " ";
+                code = code + 'text: "' + $obj.children().first().text() + '",';
+                code = code + '_id:  "' + objId + '", ';
+                code = code + 'xPos: ' + $obj.position().left + ",";
+                code = code + 'yPos: ' + $obj.position().top + " ";
             
-                // TODO: Generate code for links
-                // code = code + "links: \"[]\"";
+            // Generate code for links
+            if(allLinks[objId] != undefined){
+                var links = allLinks[objId].split(",");
+                var linksCode = "";
+                // Format : {"_id": "node01"}, {"_id": "node07"}
+                $(links).each( function(index, obj){
+                    linksCode += '{\"_id" : "' + obj + '"}';
+                    if(index + 1 < links.length){
+                        linksCode += ",";
+                    }
+                    
+                });
+                code = code + ', links: [' + linksCode + ']';
+            }
                 
             // Add this element's code with a break-rule to the entire code
             if(nodesLength == index + 1){
@@ -36,10 +65,6 @@ $(document).ready(function () {
         
         completeCode += "]";
 
-        $("#code-preview").html(completeCode);
-
-        // show or hide the section
-        $("#code-preview").slideToggle();
+        $("#code-preview").html(completeCode).slideToggle()
     });
 });
-  
